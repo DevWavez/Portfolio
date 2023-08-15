368c8a3f5490cd75aa1840e414a02292bc92db5e
DevWavez <141139262+DevWavez@users.noreply.github.com>
2023-08-15 18:21:14 +0200

    add more works

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,8 @@ import Project1 from "../assets/webD.jpeg";
 import Project2 from "../assets/Shiba.jpeg";
 import Project3 from "../assets/TinDog.jpeg";
 import Project4 from "../assets/Coventic.jpeg";
+import Project5 from "../assets/js-movie.jpg";
+import Project6 from "../assets/urban.jpg";
 
 const Work = () => {
   return (
@@ -89,7 +91,10 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/DevWavez/Spas/tree/main/TinDog%20Project">
+                <a
+                  href="https://github.com/DevWavez/Spas/tree/main/TinDog%20Project"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -113,7 +118,58 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/DevWavez/Coventic">
+                <a href="https://github.com/DevWavez/Coventic" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid item 5*/}
+          <div
+            style={{ backgroundImage: `url(${Project5})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Movie Rating Website
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/DevWavez/MovieRating"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid item 6*/}
+          <div
+            style={{ backgroundImage: `url(${Project6})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Spa Urban
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/DevWavez/Urban" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
