8c318bfaf250c1eedda25a58ccf6b963f591ca16
DevWavez <141139262+DevWavez@users.noreply.github.com>
2023-08-15 17:58:08 +0200

    add project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Project1 from "../assets/webD.jpeg";
 import Project2 from "../assets/Shiba.jpeg";
 import Project3 from "../assets/TinDog.jpeg";
+import Project4 from "../assets/Coventic.jpeg";
 
 const Work = () => {
   return (
@@ -69,6 +70,30 @@ const Work = () => {
           {/* Grid item 3*/}
           <div
             style={{ backgroundImage: `url(${Project3})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid item 4*/}
+          <div
+            style={{ backgroundImage: `url(${Project4})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover effects */}
